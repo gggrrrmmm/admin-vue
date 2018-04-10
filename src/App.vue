cc0ae9ad83205939b1cbbad8af412b8558498c79
gggrrrmmm <1727924182@qq.com>
2018-04-10 22:24:55 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <p>app component</p>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/login/login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <p>登录组件</p>
+    <form action = "">
+      <div>
+        <label for = 'username'>用户名</label>
+        <input type = "text" id = "username" v-model = "userForm.username">
+      </div>
+      <div>
+        <label for="password">密码</label>
+        <input type="password" id='password' v-model="userForm.password">
+      </div>
+      <div>
+        <button @click="login">点击登录</button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      userForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login () {
+      const res = await axios.post('http://localhost:8888/api/private/v1/login', this.userForm)
+      const data = res.data
+      if (data.meta.status === 200) {
+        this.$router.push({
+          name: 'home'
+        })
+      }
+    }
+  }
+}
+</script>
+<style>
+</style>
